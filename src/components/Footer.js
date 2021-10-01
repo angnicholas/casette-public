56a884c6a8daff85bf4440d63f8bd12cc1900579
nicholas <ang.tze.yong@gmail.com>
2021-10-01 22:03:00 +0800

    asfwe

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Nav = () => {
     <footer className={styles.footer}>
 
       <div className={styles.socialmedia}>
-        <a href="https://facebook.com"><img src="fb.png"></img></a>
-        <a href="https://instagram.com"><img src="ig.png"></img></a>
-        <a href="https://linkedin.com"><img src="in.png"></img></a>
+        <a href="https://facebook.com"><img src="%PUBLIC_URL%/fb.png"></img></a>
+        <a href="https://instagram.com"><img src="%PUBLIC_URL%/fg.png"></img></a>
+        <a href="https://linkedin.com"><img src="%PUBLIC_URL%/in.png"></img></a>
         
       </div>
       
